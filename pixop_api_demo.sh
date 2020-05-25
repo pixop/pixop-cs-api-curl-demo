ef604062049409b54bffc47458f756928a8fea84
Jon Frydensbjerg <jonf@vip.cybercity.dk>
2020-05-25 20:45:07 +0200

    Check if curl+jq exists, ensure JWT token was fetched correctly + print info about the current step

--- pixop_api_demo.sh
+++ pixop_api_demo.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+if ! [ -x "$(command -v curl)" ]; then
+  echo 'Error: curl is not installed.' >&2
+  exit 1
+fi
+if ! [ -x "$(command -v jq)" ]; then
+  echo 'Error: jq is not installed.' >&2
+  exit 1
+fi
+
 API_SITE=https://staging-api.pixop.com
 
 ACCOUNTS_PATH=$API_SITE/accounts/v1
@@ -9,8 +18,17 @@ MEDIA_PATH=$API_SITE/media/v1
 CURL_COMMON_HEADERS='-H "Content-Type: application/json" -H "Accept: application/json"'
 
 ### Step 1: Fetch JWT auth token
-AUTH_RESPONSE=$(eval "curl -s --netrc-file pixop-api-netrc $CURL_COMMON_HEADERS $ACCOUNTS_PATH/token")
+echo "Step #1: Authenticating..."
 
+AUTH_RESPONSE=$(eval "curl -s -f --netrc-file pixop-api-netrc $CURL_COMMON_HEADERS $ACCOUNTS_PATH/token")
+
+if [ $? -ne 0 ]; then
+  echo
+  echo 'Error: failed to fetch JWT token. Possible reasons: invalid credentials provided (check pixop-api-netrc) or service is unavailable.' >&2
+  exit 1
+fi
+
+echo
 echo "Auth token response..."
 echo $AUTH_RESPONSE | jq
 
@@ -25,6 +43,9 @@ CURL_ALL_HEADERS="$CURL_COMMON_HEADERS $CURL_AUTH_HEADER"
 
 
 ### Step 2: Create new project
+echo
+echo "Step #2: Creating new project..."
+
 PROJECT_RESPONSE=$(eval "curl -s -X PUT $CURL_ALL_HEADERS -d '{\"name\": \"Demo project\"}' $VIDEOS_PATH/project")
 
 echo
@@ -37,6 +58,9 @@ echo "Parsed new project id: $PROJECT_ID"
 
 
 ### Step 3: Create new video into the previously created project
+echo
+echo "Step #3: Creating new video..."
+
 VIDEO_RESPONSE=$(eval "curl -s -X PUT $CURL_ALL_HEADERS -d '{\"projectId\": \"$PROJECT_ID\"}' $VIDEOS_PATH/video")
 
 echo
@@ -52,6 +76,9 @@ echo "Parsed upload light ingested endpoint: $UPLOAD_ENDPOINT"
 
 
 ### Step 4: Upload media
+echo
+echo "Step #4: Uploading media file into video entity..."
+
 UPLOAD_RESPONSE=$(eval "curl -s -F file=@small.mp4 $CURL_AUTH_HEADER $UPLOAD_ENDPOINT")
 
 echo
@@ -68,24 +95,28 @@ echo "Parsed process video endpoint: $PROCESS_VIDEO_ENDPOINT"
 
 ### Step 5: Wait for light ingestion to complete
 echo
-echo "Waiting for ingestion to complete..."
+echo "Step #5: Waiting for ingestion to complete (this might take up to 15 seconds)..."
+echo
 
-sleep 1
+sleep 5
 
 while [ $(eval "curl -s $CURL_ALL_HEADERS $CHECK_INGEST_PROGRESS_ENDPOINT" | jq -r '.ingestionState.ingestionStatus') != "DONE" ]
 do
-  sleep 2
+  sleep 5
   echo Still waiting...
 done
 
 
 ### Step 6: Upscale video to HD 1080p
+echo
+echo "Step #6: Triggering a process video request..."
+
 PROCESS_REQUEST_BODY='{
   "scaler": "pabsr1",
   "resolution": {
     "tag": "hd_1080p"
   },
-  "clarityBoost": "low"
+  "clarityBoost": "medium"
 }'
 PROCESS_RESPONSE=$(eval "curl -s $CURL_ALL_HEADERS -d '$PROCESS_REQUEST_BODY' $PROCESS_VIDEO_ENDPOINT")
 
@@ -103,7 +134,8 @@ echo "Parsed download media endpoint: $DOWNLOAD_ENDPOINT"
 
 ### Step 7: Wait for processing to complete
 echo
-echo "Waiting for processing to complete..."
+echo "Step #7: Waiting for processing to complete (note: this can take from 15 seconds up to several minutes depending on whether compute infrastructure is available)..."
+echo
 
 sleep 5
 
@@ -115,14 +147,20 @@ done
 
 
 ### Step 8: Download processed media
-DOWNLOAD_RESPONSE=$(eval "curl -s $CURL_AUTH_HEADER $DOWNLOAD_ENDPOINT -o small_upscaled.mp4")
+echo
+echo "Step #8: Downloading processed media..."
+
+DOWNLOAD_RESPONSE=$(eval "curl -s $CURL_AUTH_HEADER $DOWNLOAD_ENDPOINT -o small_hd.mp4")
 
 echo
-echo "Download media response (empty if no errors occurred)..."
+echo "Download media response (no output if no errors occurred)..."
 echo $DOWNLOAD_RESPONSE | jq
 
 
 ### Step 9: Delete original and processed videos
+echo
+echo "Step #9: Deleting original and processed videos..."
+
 DELETE_VIDEO_RESPONSE=$(eval "curl -s -X DELETE $CURL_ALL_HEADERS $VIDEOS_PATH/video/$VIDEO_ID")
 
 echo
@@ -131,13 +169,21 @@ echo $DELETE_VIDEO_RESPONSE | jq
 
 
 ### Step 10: Delete project
+echo
+echo "Step #10: Deleting project..."
+
 DELETE_VIDEO_RESPONSE=$(eval "curl -s -X DELETE $CURL_ALL_HEADERS $VIDEOS_PATH/project/$PROJECT_ID")
 
 echo
-echo "Delete project response (empty if no errors occurred)..."
+echo "Delete project response (no output if no errors occurred)..."
 echo $DELETE_PROJECT_RESPONSE | jq
 
 
+###
+echo
+echo "Original file 'small.mp4' (560x320 pixels):       $(wc -c < small.mp4) bytes"
+echo "Processed file 'small_hd.mp4' (1890x1080 pixels): $(wc -c < small_hd.mp4) bytes"
+
 ### All done!
 echo
-echo "All done!"
+echo "All done. Have a nice day!"
